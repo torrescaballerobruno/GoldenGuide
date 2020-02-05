3f4c89de226d28392149e0a3705850781d07c751
Bruno Torres <bruno.tor.96@gmail.com>
2020-02-04 23:25:51 -0600

    adding RegisterView

---
+++ ProyectoDiploIOS/Views/RegisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterViewController.swift
+//  ProyectoDiploIOS
+//
+//  Created by Bruno Torres on 04/02/20.
+//  Copyright © 2020 Bruno Torres. All rights reserved.
+//
+
+import UIKit
+
+
+class RegisterViewController : UIViewController{
+    
+    
+}
